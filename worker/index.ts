d9f9464266d604842320037d4660292a9b54ffb5
Zack Turco <2rscrw8cmn@privaterelay.appleid.com>
2026-08-19 09:46:03 -0400

    feat: route play personnel API

--- worker/index.ts
+++ worker/index.ts
@@ -7,6 +7,7 @@ import {
   initializeAuthorization,
 } from "./authorization";
 import { handleDrillApi } from "./drills/routes";
+import { handlePlayPersonnelApi } from "./playbook/personnel-routes";
 import { handlePlaybookApi } from "./playbook/routes";
 import { handleResultsApi } from "./results/routes";
 import { handleSessionRosterApi } from "./sessions/roster-routes";
@@ -77,6 +78,9 @@ const worker = {
 
       const ownershipResponse = await handleOwnershipApi(request, env.DB, authorization);
       if (ownershipResponse) return ownershipResponse;
+
+      const playPersonnelResponse = await handlePlayPersonnelApi(request, env.DB);
+      if (playPersonnelResponse) return playPersonnelResponse;
 
       const playbookResponse = await handlePlaybookApi(request, env.DB);
       if (playbookResponse) return playbookResponse;
